bf63931bcaa13ab38b09632cf841f5345bef8648
Arch <martin.o.dinnel@gmail.com>
2024-12-20 14:27:49 +0700

    ex00

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arch <arch@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 11:37:15 by arch              #+#    #+#             */
+/*   Updated: 2024/12/20 13:56:27 by arch             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void	megaphone(std::string input)
+{
+	for(std::size_t i = 0; i < input.size(); i++)
+	{
+		unsigned char uc = static_cast<unsigned char>(input[i]);
+		if (std::islower(uc))
+			input[i] = static_cast<char>(std::toupper(uc));
+	}
+	std::cout << input;
+}
+
+int main(int argc, char **argv)
+{
+	int	i = 0;
+	if (argc > 1)
+	{
+		while (++i < argc)
+		{
+			megaphone(argv[i]);
+			if (i + 1 < argc)
+				std::cout << ' ';
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << '\n';
+	return (0);
+}
