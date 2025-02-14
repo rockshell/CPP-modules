c84361bd3332c5df691f083dbe9a4bcd19fa7e90
Arch <martin.o.dinnel@gmail.com>
2025-02-14 16:52:49 +0700

    2

--- CPP_02/ex00/Include/Fixed.hpp
+++ CPP_02/ex00/Include/Fixed.hpp
@@ -6,12 +6,30 @@
 /*   By: akulikov <akulikov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:29:24 by akulikov          #+#    #+#             */
-/*   Updated: 2025/02/05 15:29:41 by akulikov         ###   ########.fr       */
+/*   Updated: 2025/02/14 16:52:21 by akulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <string>
+
+class Fixed {
+	public:
+		Fixed () {
+			_number = 0;
+		};
+		Fixed (const &Fixed)
+		~Fixed () {};
+		
+		
+	private:
+		int _number;
+		static const int _bits = 8;
+
+		
+	
+};
 
 #endif
