f072daa27e3dd50df80f3b817234c49ffdeb62e1
Arch <martin.o.dinnel@gmail.com>
2025-05-03 18:44:01 +0700

    I hate math

--- CPP_02/ex01/Fixed.cpp
+++ CPP_02/ex01/Fixed.cpp
@@ -6,19 +6,25 @@
 /*   By: arch <arch@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:30:03 by akulikov          #+#    #+#             */
-/*   Updated: 2025/04/30 13:40:52 by arch             ###   ########.fr       */
+/*   Updated: 2025/05/03 18:35:05 by arch             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 Fixed::Fixed(void) {
-	_number = 0;
 	std::cout << "Default constructor called" << std::endl;
+	_number = 0;
 }
 
 Fixed::Fixed(const int value) {
-	
+	std::cout << "Int constructor called" << std::endl;
+	_number = value << this->_bits;
+}
+
+Fixed::Fixed(const float value) {
+	std::cout << "Float Constructor called" << std::endl;
+	_number = roundf(value * (1 << _bits));
 }
 
 Fixed::Fixed(const Fixed& src) {
@@ -37,6 +43,14 @@ Fixed::~Fixed(void) {
 	std::cout << "Destructor called" << std::endl;
 }
 
+float	Fixed::toFloat(void)const {
+	return ((float)_number / (float)(1 << _bits));
+}
+
+int	Fixed::toInt(void)const {
+	return (_number >> _bits);
+}
+
 int	Fixed::getRawBits(void)const {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_number);
@@ -45,4 +59,9 @@ int	Fixed::getRawBits(void)const {
 void Fixed::setRawBits(int const raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->_number = raw;
+}
+
+std::ostream	&operator<<(std::ostream &output, Fixed const &fixed) {
+	output << fixed.toFloat();
+	return (output);
 }
--- CPP_02/ex01/Include/Fixed.hpp
+++ CPP_02/ex01/Include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: arch <arch@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:29:24 by akulikov          #+#    #+#             */
-/*   Updated: 2025/04/30 13:40:31 by arch             ###   ########.fr       */
+/*   Updated: 2025/05/03 18:35:56 by arch             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,21 @@ class Fixed {
 	public:
 		Fixed(void);
 		Fixed(const int value);
+		Fixed(const float value);
 		Fixed(const Fixed& src);
 		Fixed& operator= (const Fixed &src);
 		~Fixed(void);
 		
 		int	getRawBits()const;
+		int	toInt(void)const;
 		void setRawBits(int const raw);
+		float toFloat(void)const;
+	
 	private:
 		int _number;
 		static const int _bits = 8;
-};
+	};
+	
+std::ostream &operator<<(std::ostream &output, Fixed const &fixed);
 
 #endif
